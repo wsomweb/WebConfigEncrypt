67e99155b7d6db2921faecc18464c904e73f83e3
Kinsey Roberts <kinzdesign@gmail.com>
2018-02-16 10:36:54 -0500

    Initial commit

---
+++ WebConfigEncrypt/WebConfigEncryptForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace edu.cwru.weatherhead.WebConfigEncrypt
+{
+    public partial class WebConfigEncryptForm : Form
+    {
+        public WebConfigEncryptForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
